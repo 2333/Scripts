5ea8a387835461c5017785accd206fe398939546
2333 <zk.amaranth@gmail.com>
2016-06-14 13:46:28 +0800

    vmware's excel process script

---
+++ vm_excel.py
@@ -0,0 +1,93 @@
+import xlrd
+import xlwt
+import re
+
+
+# from datetime
+def openfile(filename):
+    try:
+        data = xlrd.open_workbook(filename)
+        return data
+    except Exception as e:
+        print(str(e))
+
+
+def vmxlrd(filename):
+    book = openfile(filename)
+    print(book)
+    print("Worksheet name(s): ", book.sheet_names()[0])
+    sh = book.sheet_by_index(0)
+
+    print('book.nsheets', book.nsheets)
+    print('sh.name:', sh.name, 'sh.nrows:', sh.nrows, 'sh.ncols:', sh.ncols)
+    # print('A1:', sh.cell_value(rowx=0, colx=0))
+    # 如果A3的内容为中文
+    # print('A2:', sh.cell_value(167, 0))
+    name = re.match(r'[T|B|P]\-(\w+?)\-(\w+?)\-', sh.cell_value(0, 0))
+    print(name.group(1), name.group(2))
+    n = 0
+    lists = []
+    print(sh.col)
+    while n < sh.nrows:
+        name = re.search(r"[TBPt]\-([^\-]*)(\-([^\-]*)|$)", sh.cell_value(n, 0))
+        if name is None:
+            lists.append((0, 0))
+        else:
+            print(n, ': ', sh.cell_value(n, 0), '\t', name.group(1), '\t', name.group(3))
+            lists.append([sh.cell_value(n, 0), name.group(1), name.group(3)])
+        n += 1
+    print(lists[0][2])
+    vmlists = cmp_vmname('vmdata.xlsx', 'syslist.xlsx', lists)
+    a = open("lists.txt", 'w')
+    for vm in vmlists:
+        print(vm)
+        for v in vm:
+            a.write(str(v))
+            a.write('\t')
+        a.write('\n')
+    a.close()
+    #vmxlwt('vmdetail.xls', vmlists)
+
+
+def cmp_vmname(vmdata, syslist, lists):
+    vmdataxl = openfile(vmdata)
+    syslistxl = openfile(syslist)
+    vmdatash = vmdataxl.sheet_by_index(0)
+    syslistsh = syslistxl.sheet_by_index(0)
+
+    sysdict = {}
+    i = 0
+    while i < syslistsh.nrows:
+        sysdict[syslistsh.cell_value(i, 1)] = syslistsh.cell_value(i, 0)
+        i += 1
+
+    for listname in lists:
+        n = 0
+        while n < vmdatash.nrows:
+            vmname = vmdatash.cell_value(n, 0)
+            name = re.search(r"[TBPt]\-([^\-]*)(\-([^\-]*)|$)", vmname)
+            if listname[0] == vmname:
+                listname += vmdatash.row_values(n, 1, 4)
+                break
+            elif hasattr(name, "group") and listname[1] == name.group(1) and listname[2] == name.group(3):
+                listname += vmdatash.row_values(n, 1, 4)
+                break
+            n += 1
+        if listname[1] in sysdict:
+            listname.append(sysdict.get(listname[1]))
+    return lists
+
+
+def vmxlwt(filename, lists):
+    book = xlwt.Workbook()
+    sheet1 = book.add_sheet('vm_detail')
+    for i in range(len(lists)):
+        j = 0
+        for con in lists[i]:
+            sheet1.write(i, j, con)
+            j += 1
+    book.save(filename)
+
+
+if __name__ == '__main__':
+    vmxlrd("vmware.xls")
